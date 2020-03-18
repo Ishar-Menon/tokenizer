7f4f655563496650d52fbc74d699554ab718e733
Ishar-Menon <isharmenon99@gmail.com>
2020-03-18 21:10:30 +0530

    Completed token sale contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,8 @@ const zorToken = artifacts.require("zorToken");
 const tokenSale = artifacts.require("tokenSale");
 
 module.exports = function(deployer) {
-  deployer.deploy(zorToken, 1000000);
-  deployer.deploy(tokenSale, 1000000);
+  deployer.deploy(zorToken, 1000000).then(function() {
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(tokenSale, zorToken.address, tokenPrice);
+  });
 };
