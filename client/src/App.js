252c295378ba04cc4617020b56c60f02cda355e2
RKolla99 <rithvik.kolla@hotmail.com>
2020-04-13 18:31:46 +0530

    Private Route component added

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import PrivateRoute from './components/routing/PrivateRoute';
 import Alert from './components/layouts/Alert';
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
+import Dashboard from './components/layouts/Dashboard';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
@@ -29,6 +31,7 @@ function App() {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
