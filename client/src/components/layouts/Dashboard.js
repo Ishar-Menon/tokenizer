f1132af5acebde19454f62f35ae5eebd37dab187
RKolla99 <rithvik.kolla@hotmail.com>
2020-04-14 04:48:07 +0530

    Initial Dashboard setup

--- client/src/components/layouts/Dashboard.js
+++ client/src/components/layouts/Dashboard.js
@@ -9,10 +9,42 @@ const Dashboard = ({ auth: { user, loading } }) => {
     <Spinner />
   ) : (
     <Fragment>
-      <h1 className='large text-primary'>Dashboard</h1>
-      <p className='lead'>
-        <i className='fas fa-user'></i> Welcome {user && user.username}
-      </p>
+      <h1 class='large text-primary'>Dashboard</h1>
+      <div class='dashboard'>
+        <div class='user-info'>
+          <div class='img-container'>
+            <img
+              src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'
+              alt=''
+              class='round-img my-1'
+            />
+          </div>
+          <div class='user-info-group'>
+            <p class='text-primary'>Name:</p>
+            <p>{user && user.username}</p>
+          </div>
+          <div class='user-info-group'>
+            <p class='text-primary'>Email:</p>
+            <p>{user && user.email}</p>
+          </div>
+          <div class='user-info-group'>
+            <p class='text-primary'>Ethereum Wallet ID:</p>
+            <p>{user && user.Eth_wallet_id}</p>
+          </div>
+          <div class='line'></div>
+          <div class='dash-links'>
+            <a href='#' class='lead'>
+              Products Sold
+            </a>
+            <a href='#' class='lead'>
+              Products Bought
+            </a>
+            <a href='#' class='lead'>
+              Statistical Analysis
+            </a>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
