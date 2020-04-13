2ea673edc2bb4846145e0848f9578e1deee7a3aa
Ishar-Menon <isharmenon99@gmail.com>
2020-04-13 07:37:00 +0530

    Added product routes

--- server/server.py
+++ server/server.py
@@ -14,8 +14,7 @@ except ImportError:
     from flask import _request_ctx_stack as ctx_stack
 
 
-
-# Config (Rember to put in json file)
+# Config (Rember to put in a json file)
 config = {
     "mongoURI": "mongodb://localhost:27017/",
     "jwtSecret": "secretforjwttoken"
@@ -81,6 +80,7 @@ def auth(fn):
             print(e)
             return jsonify({'errors': {'general': 'Invalid token'}}), 400
         return fn(*args, **kwargs)
+    wrapper.__name__ = fn.__name__
     return wrapper
 
 
@@ -94,7 +94,7 @@ def auth(fn):
 #               email: "Email",
 #               "password" : "messi"
 #               Eth_wallet_id:"Wallet id"
-#               tokens_bought: "No of tokens bought"
+#               tokens_owned: [{productname,no of tokens}]
 #               role : "Buyer/Seller"
 #                  }
 #  Product schema : {
@@ -103,6 +103,8 @@ def auth(fn):
 #                   tokenPrice : "Price of each token"
 #                   productOwner : "Seller"
 #                   productImages : ["base64ImageString"]
+#                   productDescription : "dummy product"
+#                   shortDecription : ""
 #
 #               }
 
@@ -117,7 +119,7 @@ def db_init():
                 "email": "johndoe@example.com",
                 "password": "messi",
                 "Eth_wallet_id": "xxxxxx",
-                "tokens_bought": 0,
+                "tokens_owned": [],
                 "role": "Buyer"}
         userCollection.insert_one(user)
 
@@ -128,6 +130,7 @@ def db_init():
                    "totalTokens": 0,
                    "tokenPrice": 0,
                    "productOwner": "emailOfOwner",
+                   "shortDecription": "",
                    "productDescription": "dummy product",
                    "productImages": ["image 1", "Image 2"]
                    }
@@ -136,8 +139,8 @@ def db_init():
 # -----------------------------------------------------------------------
 #                       User routes
 
-
-@app.route('/user/create', methods=['POST'])
+# Create a new user
+@app.route('/api/user/create', methods=['POST'])
 def createUser():
 
     if not request.is_json:
@@ -151,7 +154,6 @@ def createUser():
         password = bcrypt.hashpw(params["password"].encode(), bcrypt.gensalt())
         email = params["email"]
         Eth_wallet_id = params["Eth_wallet_id"]
-        tokens_bought = 0
         role = params["role"]
 
         # Check if the user exist:
@@ -164,7 +166,7 @@ def createUser():
                    "password": password,
                    "email": email,
                    "Eth_wallet_id": Eth_wallet_id,
-                   "tokens_bought": 0,
+                   "tokens_owned": [],
                    "role": role}
     except:
         return jsonify({'errors': {'general': 'Please provide all details'}}), 400
@@ -177,16 +179,206 @@ def createUser():
     return jsonify({'msg': 'user successfully created'}), 201
 
 
-@app.route('/user', methods=['GET'])
+# Get information of the authorized user
+@app.route('/api/user/info', methods=['GET'])
 @auth
-def getAllUsers():
+def getUserInfo():
     try:
         payload = ctx_stack.top.jwtPayload
-        print(payload)
+        user = userCollection.find_one({"email": payload["email"]})
+        user.pop('_id')
+        user.pop('password')
+        return jsonify(user), 200
 
-        return jsonify({"users": "somevalue"}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'errors': {'general': 'Server error'}}), 500
+
+# Get information of the authorizeuserd user
+# tokensBought = { product_name: "",noTokensBought:0}
+@app.route('/api/user/buyTokens', methods=['POST'])
+@auth
+def buyTokens():
+
+    if not request.is_json:
+        return jsonify({'errors': {'general': 'format error (expected JSON)'}}), 400
+
+    payload = ctx_stack.top.jwtPayload
+    user = userCollection.find_one({"email": payload["email"]})
+
+    try:
+        params = request.get_json()
+        tokensBought = params["tokensBought"]
+
+        productPresent = False
+
+        # User already has some tokens for the product add more
+        for product in user["tokens_owned"]:
+            if(product["product_name"] == tokensBought["product_name"]):
+                product["noTokensBought"] += tokensBought["noTokensBought"]
+                productPresent = True
+
+        # If not bought append to list of products
+        if(not productPresent):
+            user["tokens_owned"].append(tokensBought)
+
+        newvalues = {"$set": {"tokens_owned": user["tokens_owned"]}}
+        userCollection.update_one({"email": payload["email"]}, newvalues)
+
+        return jsonify({"msg": "successful update"}), 200
+    except:
+        return jsonify({'errors': {'general': 'Please provide all details'}}), 400
+
+
+# Testing route 1
+@app.route('/user', methods=['GET'])
+def getAllUsers():
+    try:
+        users = userCollection.find()
+        userInfo = {"users": []}
+        for user in users:
+            user.pop("_id")
+            userInfo["users"].append(user)
+
+        return jsonify(userInfo), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'errors': {'general': 'Server error'}}), 500
+
+
+# -----------------------------------------------------------------------
+#                       Product routes
+
+# Create a new product(done by authorized user)
+@app.route('/api/product/create', methods=['POST'])
+@auth
+def createProduct():
+
+    if not request.is_json:
+        return jsonify({'errors': {'general': 'format error (expected JSON)'}}), 400
+
+    payload = ctx_stack.top.jwtPayload
+    productOwner = payload["email"]
+
+    try:
+        params = request.get_json()
+        product_name = params["product_name"]
+        totalTokens = params["totalTokens"]
+        tokenPrice = params["tokenPrice"]
+        shortDecription = params["shortDecription"]
+        productDescription = params["productDescription"]
+        productImages = params["productImages"]
+
+        # Check if the product with same name exist
+        # (Can be changed to check using a product id later):
+        users = userCollection.find({"product_name": product_name})
+
+        if(users.count() != 0):
+            return jsonify({'errors': {'general': 'Product with same name exist already exist !'}}), 400
+
+        newProduct = {"product_name": product_name,
+                      "totalTokens": totalTokens,
+                      "tokenPrice": tokenPrice,
+                      "productOwner": productOwner,
+                      "shortDecription": shortDecription,
+                      "productDescription": productDescription,
+                      "productImages": productImages
+                      }
+    except:
+        return jsonify({'errors': {'general': 'Please provide all details'}}), 400
+
+    try:
+        productCollection.insert_one(newProduct)
     except:
         return jsonify({'errors': {'general': 'Server error'}}), 500
+
+    return jsonify({'msg': 'product successfully created'}), 201
+
+# List products with their information
+# (Inforamtion passed from client - items to skip)
+@app.route('/api/product/list', methods=['GET'])
+def listProduct():
+
+    if not request.is_json:
+        return jsonify({'errors': {'general': 'format error (expected JSON)'}}), 400
+
+    try:
+        params = request.get_json()
+        skipAmount = params["skipAmount"]
+
+    except:
+        return jsonify({'errors': {'general': 'Please provide skip amount'}}), 400
+
+    try:
+        products = productCollection.find().skip(skipAmount).limit(10)
+    except:
+        return jsonify({'errors': {'general': 'Server error'}}), 500
+
+    productInfo = {"products": []}
+
+    for product in products:
+        product.pop("_id")
+        print(product)
+        productInfo["products"].append(product)
+
+    return jsonify(productInfo), 200
+
+
+# List products with their information
+# (Inforamtion passed from client - items to skip)
+@app.route('/api/product/listUser', methods=['GET'])
+@auth
+def listUsersProduct():
+
+    if not request.is_json:
+        return jsonify({'errors': {'general': 'format error (expected JSON)'}}), 400
+
+    payload = ctx_stack.top.jwtPayload
+    productOwner = payload["email"]
+
+    try:
+        params = request.get_json()
+        skipAmount = params["skipAmount"]
+
+    except:
+        return jsonify({'errors': {'general': 'Please provide skip amount'}}), 400
+
+    try:
+        products = productCollection.find(
+            {"productOwner": productOwner}
+        ).skip(skipAmount).limit(10)
+    except:
+        return jsonify({'errors': {'general': 'Server error'}}), 500
+
+    productInfo = {"products": []}
+
+    for product in products:
+        product.pop("_id")
+        productInfo["products"].append(product)
+
+    return jsonify(productInfo), 200
+
+# Testing route 2
+@app.route('/product', methods=['GET'])
+def getAllProducts():
+    try:
+        products = productCollection.find()
+        productInfo = {"products": []}
+        for product in products:
+            product.pop("_id")
+            productInfo["products"].append(product)
+        return jsonify(productInfo), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'errors': {'general': 'Server error'}}), 500
+# -----------------------------------------------------------------------
+#                       Price prediction routes
+# List products with their information
+# (Inforamtion passed from client - items to skip)
+@app.route('/api/predict', methods=['GET'])
+@auth
+def predict():
+    pass
 
 
 if __name__ == '__main__':
