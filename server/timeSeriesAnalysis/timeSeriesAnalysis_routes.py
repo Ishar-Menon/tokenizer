a0856e0256e7657d58300482fc9bc69f0fdfd5df
AmruthKarnam <amruthkarnam@gmail.com>
2020-04-14 19:27:21 +0530

    Flask route updated

--- server/timeSeriesAnalysis/timeSeriesAnalysis_routes.py
+++ server/timeSeriesAnalysis/timeSeriesAnalysis_routes.py
@@ -1,12 +1,78 @@
 from flask import Blueprint
 from flask import current_app as app
 from middleware.middleware import auth
-
+from pandas import read_csv
+from matplotlib import pyplot
+from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
+from statsmodels.tsa.arima_model import ARIMA
+import statsmodels.api as sm
+import pandas as pd
+import time
+import threading
+from sklearn.metrics import mean_squared_error
 timeSeries_bp = Blueprint('timeSeries_bp', __name__)
+
+def updateTestAndTrain():
+    global train
+    global test 
+    global value
+    while(True):
+        time.sleep(value)
+        dataRow=test.head(1)
+        train =pd.concat([train,dataRow],axis = 0).reset_index(drop=True)
+        test= test.iloc[1:]
+    
+
+
+
+
+Foreign_Exchange_Rates = pd.read_csv("/kaggle/input/dataset/Foreign_Exchange_Rates.csv")
+df=Foreign_Exchange_Rates[['Time Serie','AUSTRALIA - AUSTRALIAN DOLLAR/US$']]
+gdf = df[df['AUSTRALIA - AUSTRALIAN DOLLAR/US$'] == 'ND']
+df=pd.concat([df, gdf, gdf]).drop_duplicates(keep=False)
+df = df.reset_index(drop=True)
+#plot_pacf(df['AUSTRALIA - AUSTRALIAN DOLLAR/US$'], lags=5)
+#plot_acf(df['AUSTRALIA - AUSTRALIAN DOLLAR/US$'],lags=3000)
+df.rename(columns = {'Time Serie':'index'}, inplace = True)
+df.rename(columns = {'AUSTRALIA - AUSTRALIAN DOLLAR/US$':'value'}, inplace = True)
+df=pd.DataFrame(df.value.astype(float))
+X = df.value
+size = int(len(X) * 0.66)
+train, test = X[0:size], X[size:len(X)]
+value = 300
+t1 = threading.Thread(target=updateTestAndTrain, args=())
+t1.start()
+
 
 # List products with their information
 # (Inforamtion passed from client - items to skip)
 @timeSeries_bp.route('/api/predict', methods=['GET'])
 @auth
-def predict():
-    pass
+def predict(flag):
+    global train
+    history = [x for x in train]
+    model = ARIMA(history, order=(1,0,0))
+    model_fit = model.fit(disp=0)
+    if(flag==0):
+        output = model_fit.forecast()
+        yhat = output[0]
+        return yhat
+
+    elif(flag==1):
+        output = model_fit.forecast(7)
+        yhat = output[0]
+        return yhat
+
+    elif(flag==2):
+        output = model_fit.forecast(30)
+        yhat = output[0]
+        return yhat
+    
+    else:
+        return -1
+
+
+
+
+
+
